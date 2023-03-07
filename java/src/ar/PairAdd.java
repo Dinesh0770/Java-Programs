f8689846081a053f75039051c07bae32a6d6cb43
HEMANT <HEMANT@DESKTOP-Q1KPBM0>
2023-03-07 21:43:48 +0530

    Adding prog

--- java/src/ar/PairAdd.java
+++ java/src/ar/PairAdd.java
@@ -3,14 +3,14 @@ package ar;
 public class PairAdd {
 
 	public static void main(String[] args) {
-		int arr[] = { 10, 20, 30, 40 };
-		int sum = 30;
+		int arr[] = { 10, 20, 30, 40, 50 };
+		int sum = 50;
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 1; j < arr.length; j++) {
 
 				if (arr[i] + arr[j] == sum) {
 
-					System.out.println(arr[i] + "+" + arr[j]);
+					System.out.println(arr[i] + "+" + arr[j] + "=" + sum);
 
 				}
 			}
